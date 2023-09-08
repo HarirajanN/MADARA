8291b4765acc783972efae64bda2a72e5d64e8ce
Harirajan N <139871782+HarirajanN@users.noreply.github.com>
2023-09-08 17:31:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function Dark(){
+    document.querySelector('body').style.color = 'white'
+    document.querySelector('body').style.backgroundColor = 'black'
+    document.getElementById('menu').style.backgroundColor = 'black'
+}
